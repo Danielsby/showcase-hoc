a3b888595b34a9e85f2eeda6157062f291157d35
Daniel By <daniel.by@spk.no>
2022-06-30 12:56:08 +0200

    refactor: removed console errors

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,5 +1,5 @@
 export const Hello = (props) => {
  return (
-     <h1 onClick={() => alert("Hello from component")} {...props}>Hello {props.userName}</h1>
+     <h1 onClick={() => {alert("Hello from component")}} {...props}>Hello {props.username}</h1>
  )
 }
--- src/Hoc.jsx
+++ src/Hoc.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 
 export function HocComponent(WrappedComponent) {
-    const lastName = 'Henriksen';
-
     return class extends React.Component {
         constructor(props) {
             super(props);
             this.state = {
-                userName: ''
+                username: ''
             }
             this.onClickHandler = this.onClickHandler.bind(this);
         }
 
         onClickHandler() {
             alert("Hello from Hoc, we changed the name for you");
-            this.setState({userName: "Pernille"});
+            this.setState({username: "Pernille"});
         }
 
         render() {
@@ -25,8 +23,7 @@ export function HocComponent(WrappedComponent) {
 
             return (
                 <WrappedComponent
-                    userName={this.state.userName}
-                    lastName={lastName}
+                    username={this.state.username}
                     style={{backgroundColor: "lightblue"}}
                     {...this.props}
                     {...this.state}
